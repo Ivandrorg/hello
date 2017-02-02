e3fb7d7a3eb46afebc6027ccd9b9cf498ee16056
ivandro <usuario@DESKTOP-8LIEST6>
2017-02-02 18:50:13 -0300

    Criado o metodo static para imprimir hello wold.

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,8 +1,15 @@
 
 public class Hello {
-
-	public static void main(String[] args) {
+	
+	public static void hello(){
 		System.out.println("Olá Mundo!");
+	}
+	public static void main(String[] args) {
+		hello();
+		
+		
+		
+				
 	}
 
 }
