0e2a11fbd7d9acbca9f63004485f82a511c9f8ce
Michell Jacks de Oliveira <michelljacks@gmail.com>
2017-02-02 19:01:54 -0300

    Criação do método Hello2.

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,11 +4,14 @@ public class Hello {
 	public static void hello(){
 		System.out.println("Olá Mundo!");
 	}
+	public static void hello2(){
+		System.out.println("Hello World!");
+	}
 	public static void main(String[] args) {
 		hello();
+		hello2();
 		
-		
-		
+	
 				
 	}
 
