e9d287594c25eb40ca0aba03014edd51095e30b4
Ivandrorg <ivandrorg@gmail.com>
2017-02-02 20:27:53 -0200

    Update Hello.java

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -7,13 +7,11 @@ public class Hello {
 	public static void hello2(){
 		System.out.println("Hello World!");
 	}
-	public static void hello3(){
-		System.out.println("Show de bola !!!!!!!!!!!!!!!!!!");
-	}
+	
 	public static void main(String[] args) {
 		hello();
 		hello2();
-		hello3();
+		
 	
 				
 	}
